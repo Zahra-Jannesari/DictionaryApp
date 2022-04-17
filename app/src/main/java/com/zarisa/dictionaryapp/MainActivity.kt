79e2b9fdcb2f1f05150957a9d75ca7c6dd345073
Zarisa <jannesari.zarisa@gmail.com>
2022-04-17 15:43:34 +0430

    fix splash screen code in MainActivity.kt

--- app/src/main/java/com/zarisa/dictionaryapp/MainActivity.kt
+++ app/src/main/java/com/zarisa/dictionaryapp/MainActivity.kt
@@ -2,10 +2,26 @@ package com.zarisa.dictionaryapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.zarisa.dictionaryapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+        binding.iconSplashScreen.let {
+            it.alpha = 0f
+            it.animate().setDuration(3000).alpha(1f).withEndAction {
+                binding.fragmentContainerView.visibility = View.VISIBLE
+                binding.iconSplashScreen.visibility = View.GONE
+                overridePendingTransition(
+                    android.R.anim.fade_in,
+                    android.R.anim.fade_out
+                )
+            }
+        }
     }
 }

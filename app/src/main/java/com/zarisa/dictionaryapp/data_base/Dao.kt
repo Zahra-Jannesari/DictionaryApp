06d5b107c218b0210cdaa873870db3410f220f10
Zarisa <jannesari.zarisa@gmail.com>
2022-04-17 16:39:35 +0430

    add doa with primary queries

---
+++ app/src/main/java/com/zarisa/dictionaryapp/data_base/Dao.kt
@@ -0,0 +1,19 @@
+package com.zarisa.dictionaryapp.data_base
+
+import androidx.room.*
+
+@Dao
+interface WordDao{
+    @Query("SELECT * FROM Word WHERE englishWord in (:word)")
+    fun getEnglishWord(word:String):Word
+    @Query("SELECT * FROM Word WHERE persianWord in (:word)")
+    fun getPersianWord(word:String):Word
+    @Query("SELECT count(*) FROM Word")
+    fun getCountOfWords():Int
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertOrUpdate(word:Word)
+    @Update
+    fun updateWord(word:Word)
+    @Delete
+    fun deleteWord(word:Word)
+}

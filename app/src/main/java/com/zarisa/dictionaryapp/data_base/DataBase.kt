24008af71f46359279316c2ce8722a933c98a308
Zarisa <jannesari.zarisa@gmail.com>
2022-04-19 10:38:38 +0430

    add dataBase file

---
+++ app/src/main/java/com/zarisa/dictionaryapp/data_base/DataBase.kt
@@ -0,0 +1,33 @@
+package com.zarisa.dictionaryapp.data_base
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Word::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun questionDao(): WordDao
+
+    companion object {
+        var INSTANCE: AppDatabase? = null
+
+        fun getAppDataBase(context: Context): AppDatabase? {
+            if (INSTANCE == null){
+                synchronized(AppDatabase::class){
+                    INSTANCE =
+                        Room.databaseBuilder(context.applicationContext,
+                            AppDatabase::class.java, "myDB")
+                            .allowMainThreadQueries()
+                            .build()
+                }
+            }
+            return INSTANCE
+        }
+
+        fun destroyDataBase(){
+            INSTANCE = null
+        }
+    }
+
+}

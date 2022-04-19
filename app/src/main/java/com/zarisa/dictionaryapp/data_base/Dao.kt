f02572c0f6f5cace5f00337e17390e97876a8b1d
Zarisa <jannesari.zarisa@gmail.com>
2022-04-19 12:07:41 +0430

    set search edit text in main fragment view

--- app/src/main/java/com/zarisa/dictionaryapp/data_base/Dao.kt
+++ app/src/main/java/com/zarisa/dictionaryapp/data_base/Dao.kt
@@ -4,10 +4,10 @@ import androidx.room.*
 
 @Dao
 interface WordDao{
-    @Query("SELECT * FROM Word WHERE englishWord in (:word)")
-    fun getEnglishWord(word:String):Word
-    @Query("SELECT * FROM Word WHERE persianWord in (:word)")
-    fun getPersianWord(word:String):Word
+    @Query("SELECT * FROM Word WHERE englishWord in (:word) or persianWord in (:word)")
+    fun getWord(word:String):Word
+//    @Query("SELECT * FROM Word WHERE persianWord in (:word)")
+//    fun getPersianWord(word:String):Word
     @Query("SELECT count(*) FROM Word")
     fun getCountOfWords():Int
     @Insert(onConflict = OnConflictStrategy.REPLACE)

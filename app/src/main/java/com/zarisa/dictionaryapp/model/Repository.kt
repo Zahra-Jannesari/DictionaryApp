38aff69aede6c8bad9eac75d6c10e1e9e10626b6
Zarisa <jannesari.zarisa@gmail.com>
2022-04-19 23:08:50 +0430

    add repository

---
+++ app/src/main/java/com/zarisa/dictionaryapp/model/Repository.kt
@@ -0,0 +1,20 @@
+package com.zarisa.dictionaryapp.model
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import com.zarisa.dictionaryapp.data_base.AppDatabase
+import com.zarisa.dictionaryapp.data_base.Word
+import com.zarisa.dictionaryapp.data_base.WordDao
+
+object Repository {
+    lateinit var wordDao  : WordDao
+    fun initDB(context : Context){
+        wordDao = AppDatabase.getAppDataBase(context).wordDao()
+    }
+    fun getCountOfWords():LiveData<Int>{
+        return wordDao.getCountOfWords()
+    }
+    fun getWord(word:String):Word?{
+        return wordDao.getWord(word)
+    }
+}

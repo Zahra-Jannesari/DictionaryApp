68fb1aa057f4e06025cc7321c867b1697c150729
Zarisa <jannesari.zarisa@gmail.com>
2022-04-19 23:09:02 +0430

    add viewModel

---
+++ app/src/main/java/com/zarisa/dictionaryapp/model/MainViewModel.kt
@@ -0,0 +1,20 @@
+package com.zarisa.dictionaryapp.model
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.zarisa.dictionaryapp.data_base.Word
+
+class MainViewModel(app: Application) : AndroidViewModel(app) {
+    val wordCounterLiveData=MutableLiveData<Int>(0)
+    init {
+        Repository.initDB(app.applicationContext)
+        updateWordCounter()
+    }
+    fun getWord(word: String): Word? {
+        return Repository.getWord(word)
+    }
+    fun updateWordCounter(){
+        wordCounterLiveData.value=Repository.getCountOfWords().value
+    }
+}

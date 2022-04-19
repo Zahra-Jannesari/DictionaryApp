1b420fc77e87f17811488e46f062230593223201
Zarisa <jannesari.zarisa@gmail.com>
2022-04-19 23:10:43 +0430

    update database syntax

--- app/src/main/java/com/zarisa/dictionaryapp/data_base/DataBase.kt
+++ app/src/main/java/com/zarisa/dictionaryapp/data_base/DataBase.kt
@@ -7,26 +7,25 @@ import androidx.room.RoomDatabase
 
 @Database(entities = [Word::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
-    abstract fun questionDao(): WordDao
+    abstract fun wordDao(): WordDao
 
     companion object {
+        @Volatile
         var INSTANCE: AppDatabase? = null
-
-        fun getAppDataBase(context: Context): AppDatabase? {
-            if (INSTANCE == null){
-                synchronized(AppDatabase::class){
-                    INSTANCE =
-                        Room.databaseBuilder(context.applicationContext,
-                            AppDatabase::class.java, "myDB")
-                            .allowMainThreadQueries()
-                            .build()
-                }
+        fun getAppDataBase(context: Context): AppDatabase {
+            val _Instance = INSTANCE
+            if (_Instance != null)
+                return _Instance
+            synchronized(AppDatabase::class) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java, "myDB"
+                )
+                    .allowMainThreadQueries()
+                    .build()
+                INSTANCE = instance
+                return instance
             }
-            return INSTANCE
-        }
-
-        fun destroyDataBase(){
-            INSTANCE = null
         }
     }
 

2227458ac802a0836f11ba06a5b7b0ea0f890cb2
Zarisa <jannesari.zarisa@gmail.com>
2022-04-24 20:43:52 +0430

    add word adaptor class

---
+++ app/src/main/java/com/zarisa/dictionaryapp/adapters/WordListAdapter.kt
@@ -0,0 +1,39 @@
+package com.zarisa.dictionaryapp.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.zarisa.dictionaryapp.data_base.Word
+import com.zarisa.dictionaryapp.databinding.WordListItemRowBinding
+
+class WordListAdapter(var wordList: MutableList<Word> = mutableListOf()) : RecyclerView.Adapter<WordListAdapter.Holder>() {
+    inner class Holder(val binding: WordListItemRowBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: Word) {
+            try {
+                binding.itemEnglishWord.text = item.englishWord
+                binding.itemPersianWord.text = item.persianWord
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val binding= WordListItemRowBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return Holder(binding)
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.bind(wordList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return wordList.size
+    }
+    fun setDate(newList: List<Word>){
+        wordList.clear()
+        wordList.addAll(newList)
+        notifyDataSetChanged()
+    }
+}

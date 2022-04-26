7fc7ec4517627cc87cbb552460b4fa3cef404b09
Zarisa <jannesari.zarisa@gmail.com>
2022-04-26 17:40:04 +0430

    add new queries

--- app/src/main/java/com/zarisa/dictionaryapp/data_base/Dao.kt
+++ app/src/main/java/com/zarisa/dictionaryapp/data_base/Dao.kt
@@ -6,16 +6,20 @@ import com.zarisa.dictionaryapp.model.Repository
 
 @Dao
 interface WordDao{
-    @Query("SELECT * FROM Word WHERE englishWord in (:word) or persianWord in (:word)")
+    @Query("SELECT * FROM Word WHERE englishWord in (:word) or persianWord in (:word) LIMIT 1")
     fun getWord(word:String):Word?
+    @Query("SELECT * FROM Word WHERE wordId in (:wordID)")
+    fun getWordById(wordID:Int):Word
     @Query("SELECT count(*) FROM Word")
     fun getCountOfWords():LiveData<Int>
+    @Query("SELECT count(*) FROM Word")
+    fun wordListSize():Int
     @Query("SELECT * From Word")
     fun getWordsList():List<Word>
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertOrUpdate(word:Word)
-//    @Query("REPLACE Into Word with ")
-//    fun updateWord(oldWord:Word,editedWord:Word)
+    fun insert(word:Word)
+    @Update
+    fun updateWord(word:Word)
     @Delete
     fun deleteWord(word:Word)
 }

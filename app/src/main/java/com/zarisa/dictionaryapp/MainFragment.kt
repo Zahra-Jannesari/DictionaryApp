8f7a18c36f1927beade334c933b5b6962164705d
Zarisa <jannesari.zarisa@gmail.com>
2022-04-26 17:48:45 +0430

    refactor code in MainFragment for better navigating

--- app/src/main/java/com/zarisa/dictionaryapp/MainFragment.kt
+++ app/src/main/java/com/zarisa/dictionaryapp/MainFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.bundleOf
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -15,14 +14,16 @@ import com.zarisa.dictionaryapp.databinding.FragmentMainBinding
 import com.zarisa.dictionaryapp.model.MainViewModel
 import java.util.*
 
+const val EDIT = "addOrEdit"
+const val WORDID = "wordId"
 
 class MainFragment : Fragment() {
 
     private lateinit var binding: FragmentMainBinding
-    val wordListAdapter=WordListAdapter(){
-        it->goToWordDetailFragment(it)
+    private val wordListAdapter = WordListAdapter {
+        goToWordDetailFragment(it)
     }
-    val viewModel:MainViewModel by viewModels()
+    private val viewModel: MainViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -34,33 +35,43 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel.wordCounterLiveData.observe(viewLifecycleOwner){
-            binding.textViewWordsCounter.text= it?.toString() ?: "0"
+        viewModel.wordCounterLiveData.observe(viewLifecycleOwner) {
+            binding.textViewWordsCounter.text = it?.toString() ?: "0"
         }
-        binding.recyclerview.adapter=wordListAdapter
+        binding.recyclerview.adapter = wordListAdapter
         wordListAdapter.setDate(viewModel.getWordsList())
         onClicks()
     }
-    fun goToWordDetailFragment(word:String){
-        var bundle= bundleOf("word" to word)
-        findNavController().navigate(R.id.action_mainFragment_to_wordDetailFragment,bundle)
+
+    private fun goToWordDetailFragment(wordId: Int) {
+        val bundle = bundleOf(
+            WORDID to wordId
+        )
+        findNavController().navigate(R.id.action_mainFragment_to_wordDetailFragment, bundle)
     }
+
     private fun onClicks() {
         binding.textFieldSearch.setEndIconOnClickListener {
-            val searchedWord = binding.editTextSearch.text.toString().lowercase(Locale.getDefault())
-            if (viewModel.getWord(searchedWord) != null) {
-                goToWordDetailFragment(searchedWord)
+            val searchedWord = viewModel.getWord(
+                binding.editTextSearch.text.toString().lowercase(Locale.getDefault())
+            )
+            if (searchedWord != null) {
+                goToWordDetailFragment(searchedWord.wordId)
             } else {
                 MaterialAlertDialogBuilder(requireContext())
-                    .setMessage("The word you are looking for has not registered before." +
-                            "\nTry adding the word with button `+` below.")
+                    .setMessage(
+                        "The word has not registered before.\nTry adding the word with button `+` below."
+                    )
                     .setTitle("Word not found!")
                     .setPositiveButton("got it!") { dialog, _ -> dialog.dismiss() }
                     .show()
             }
         }
         binding.fabAdd.setOnClickListener {
-            findNavController().navigate(R.id.action_mainFragment_to_addWordFragment)
+            val bundle = bundleOf(
+                EDIT to false
+            )
+            findNavController().navigate(R.id.action_mainFragment_to_addWordFragment, bundle)
         }
     }
 }
